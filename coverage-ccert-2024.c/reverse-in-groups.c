d85c9ca71e80bb6584518ca488d3b7644de5b60c
Cyanineee-24 <scoutchampion123@gmail.com>
2025-04-23 21:57:56 +0800

    added solution for reverse in groups without pointers

--- coverage-ccert-2024.c/reverse-in-groups.c
+++ coverage-ccert-2024.c/reverse-in-groups.c
@@ -40,3 +40,29 @@ void reverseK(int *arr, int size, int k)
     for (int i = 0; i < size; i++)
         printf("%d ", *(arr + i));
 }
+void reverseKCharles(int* arr, int size, int k)
+{
+    {
+        for (int i = 0; i < size; i += k)
+        {
+            int left = i;
+            int right;
+            // to handle case when k is not multiple of n
+            if(i+k-1<size-1)
+            right = i+k-1;
+            else
+            right = size-1;
+    
+            // reverse the sub-array [left, right]
+            while (left < right)
+                {
+                    // swap
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                    left++;
+                    right--;
+                }
+        }
+    }
+}
